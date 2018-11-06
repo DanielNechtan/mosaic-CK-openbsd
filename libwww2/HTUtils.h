1ace84d5afb8913d731320ab28e4f373812fbd4e
int16h <int16h@openbsd.space>
2018-11-06 15:31:21 +0000

    Disabled TLS

--- libwww2/HTUtils.h
+++ libwww2/HTUtils.h
@@ -195,7 +195,7 @@ Booleans
 
 #define TCP_PORT 80     /* Allocated to http by Jon Postel/ISI 24-Jan-92 */
 #define TCPTLS_PORT 443
-#define TLS 1
+#define TLS 0
 
 /*      Inline Function WHITE: Is character c white space? */
 /*      For speed, include all control characters */
